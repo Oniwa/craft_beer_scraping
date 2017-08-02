6391d06c49c4df6cc4ea927800d9954f8995bfb1
John Barksdale <jdbarks@gmail.com>
2017-08-02 16:56:46 -0500

    Finished http://www.jeannicholashould.com/profiling-a-dataset-of-craft-beers.html
    Should look into running this in an ipython notebook

--- profile_data.py
+++ profile_data.py
@@ -1,3 +1,5 @@
+from multiprocessing import freeze_support
+
 import pandas as pd
 import pandas_profiling
 import seaborn as sns
@@ -21,3 +23,74 @@ beers_and_breweries = pd.merge(beers,
                                )
 
 
+# Function to determine custom categories of data
+def get_var_category(series):
+    unique_count = series.nunique(dropna=False)
+    total_count = len(series)
+    if pd.api.types.is_numeric_dtype(series):
+        return 'Numerical'
+    elif pd.api.types.is_datetime64_dtype(series):
+        return 'Date'
+    elif unique_count == total_count:
+        return 'Text (Unique)'
+    else:
+        return 'Categorical'
+
+
+# Print the custom categories for a data frame
+def print_categories(df):
+    for column_name in df.columns:
+        print(column_name, ": ", get_var_category(df[column_name]))
+
+# print_categories(beers)
+# print_categories(breweries)
+
+# Return number of observations in a series regardless if data is
+# missing or null
+length = len(beers['ibu'])
+# print(length)
+
+# Return number of non-NA/non-Null observations in a series
+count = beers['ibu'].count()
+# print(count)
+
+# Find percentage of missing values
+number_of_missing_values = length - count
+pct_of_missing_values = float(number_of_missing_values / length)
+pct_of_missing_values = "{0:.1f}%".format(pct_of_missing_values*100)
+# print(pct_of_missing_values)
+
+# Find Min and Max value of IBU
+# print("Minimum value: ", beers['ibu'].min())
+# print("Maximum value: ", beers['ibu'].max())
+
+# Find the most frequent IBU
+# print(beers['ibu'].mode())
+
+# Find the mean of the IBU
+# print(beers['ibu'].mean())
+
+# Find the IBU in the middle of the dataset
+# print(beers['ibu'].median())
+
+# Find the standard deviation of the IBU
+# print(beers['ibu'].std())
+
+# Split the data into a quartile
+# print(beers['ibu'].quantile([.25, .5, .75]))
+
+# Create a distribution plot
+# should display in an ipython notebook
+sns.distplot(beers["ibu"].dropna())
+
+# Perform the Pearson correlation on the dataset
+# print(beers[['abv', 'ibu', 'ounces']].corr())
+
+# Summarize the categorical data
+# print(beers[["name", "style"]].describe())
+
+if __name__ == "__main__":
+    freeze_support()
+    # Use an out of the box data profiling tool
+    # Looks like this is another thing that only works in ipython notebooks
+    pandas_profiling.ProfileReport(beers_and_breweries)
